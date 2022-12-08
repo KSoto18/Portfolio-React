8a1f17672db6d2267dc81cd8d05de34570f33c7a
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-12-07 23:47:18 -0500

    added more styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 import './styles.css';
 
+import { BsStars } from "react-icons/bs";
+
 
 const Header = () => {
 
     return (
 
+        
         <div className='devName'> 
             
-            <h1 className='khris'>KHRIS SOTO</h1>
+            <h1 className='khris'> Khris Soto || Web Developer</h1>
 
         </div>
+  
 
     );
 };
