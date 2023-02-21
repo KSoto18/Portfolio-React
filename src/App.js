1619757288da26a1aef1768958508bbe347fd584
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-02-21 12:05:47 -0500

    added comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+// Main Imports
 import React, { useState } from 'react';
 
+// Styling
 import 'animate.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../src/App.css'
 
+// Components/Functions
 import Body from './pages/Body';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { OpeningTitle, TitleAway }  from './components/OpeningTitle'
 
+
+// Entrance
 export function Title() {
   return (
     <OpeningTitle />
@@ -19,9 +24,9 @@ export function NoTitle() {
   return (
     <TitleAway />
   )
-}
+};
 
-
+// Main Page
 export function App() {
   return (
 
@@ -42,4 +47,4 @@ export function App() {
     </div>
 
   );
-}
+};
