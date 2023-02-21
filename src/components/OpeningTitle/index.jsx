b5761e65d2c37d38c79658d2ca60f1c268f7ad1b
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-02-21 12:01:40 -0500

    more styling to OpeningTitle

--- src/components/OpeningTitle/index.jsx
+++ src/components/OpeningTitle/index.jsx
@@ -7,14 +7,16 @@ export function OpeningTitle() {
 
     return (
 
-        <div className='titleBox'>
+        <div className='titleBackground'>
 
-            <h1 class="animate__animated animate__bounceInLeft"> Khris Soto </h1>
-            <h1 class="animate__animated animate__bounceInRight"> Web Developer </h1>
+            <div className='titleBox'>
+
+                <h1 class="animate__animated animate__bounceInLeft"> Khris Soto </h1>
+                <h1 class="animate__animated animate__bounceInRight"> Web Developer </h1>
 
 
+            </div>
         </div>
-
 
     );
 };
@@ -24,11 +26,15 @@ export function TitleAway() {
 
     return (
 
-        <div className='titleBox'>
+        <div className='titleBackground'>
 
-            <h1 class="animate__animated animate__bounceOutUp"> Khris Soto </h1>
-            <h1 class="animate__animated animate__bounceOutDown"> Web Developer </h1>
+            <div className='titleBox'>
 
+                <h1 class="animate__animated animate__bounceOutUp"> Khris Soto </h1>
+                <h1 class="animate__animated animate__bounceOutDown"> Web Developer </h1>
+
+
+            </div>
         </div>
     )
 
