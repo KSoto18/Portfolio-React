4fdf0155584b83d24fc2ba78ea388601abbf3941
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-02-23 12:24:52 -0500

    added codeacademy to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,6 +5,7 @@ import './styles.css';
 import { BsGithub } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
+import { SiCodecademy } from "react-icons/si";
 
 const Footer = () => {
 
@@ -15,6 +16,7 @@ const Footer = () => {
             <a href='https://github.com/KSoto18' target='new' className='footerBtn'><BsGithub /></a>
             <a href='https://www.linkedin.com/in/khristel-soto-9468a6259/' target='new' className='footerBtn'><BsLinkedin /></a>
             <a href='https://www.instagram.com/kapt_sprinkles/?hl=en' target='new' className='footerBtn'><BsInstagram /></a>
+            <a href='https://www.codecademy.com/profiles/KSoto18' target='new' className='footerBtn'><SiCodecademy /></a>
         </div>
 
             <p className='footer-text'> Ⓒ KSoto18 2022/2023</p>
