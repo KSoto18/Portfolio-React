4975ae54cb50c19976416ecdc22c0d5c3bd0d33f
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-05-04 18:16:00 -0400

    fixing typos

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -21,7 +21,7 @@ const About = () => {
 
                     <p>I am a certified Full Stack Web Develper from Orlando, FL. </p>
 
-                    <p> Coming from a background in Hospitality and Service of over 10 years, I look to combine my passion for technology and design with my expiriences to make eye-catching, user-friendy, and accesible websites and applications.</p>
+                    <p> Coming from a background in Hospitality and Service of over 10 years, I look to combine my passion for technology and design with my experiences to make eye-catching, user-friendly, and accesible websites and applications.</p>
 
                     <p>I have recieved my education from the UCF Coding Boot Camp
                         Full Stack Web Development Program in December of 2022. While my education was focused on the MERN Stack I am still exploring other technologies and frameworks that catch my interest.</p>
